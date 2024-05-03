c09d518e49d0800f8ab1f66a7831d30d53b18058
AnneSophieH <xcd811@alumni.ku.dk>
2024-05-03 21:42:07 +0200

    Updated py-file with solvers Updated notebook with additional math for introduction

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -26,4 +26,11 @@ def solve_ss_z(sE, eps, phi, n, g, beta, s, delta, alpha):
 obj_zss = lambda zss: zss-(1/(1-sE))**(eps+phi)*(1/((1+n)(1+g)))**beta*(s+(1-delta)*zss)**(1-alpha)*zss**alpha
 result = optimize.root_scalar(obj_z,bracket=[0.1,100],method='brentq')
 
-print('the steady state for z is',result.root)
+print('The steady state for z in the Solow model with an exhaustable resource and climate change is',result.root)
+
+def solve_ss_k(alpha, s, delta, g, n):
+f = lambda k: k**alpha
+obj_kss = lambda kss: kss - (s*f(kss) + (1-delta)*kss)/((1+g)*(1+n))
+result = optimize.root_scalar(obj_kss,bracket=[0.1,100],method='brentq')
+
+print('The steady state for k in the standard Solow model is',result.root)   
