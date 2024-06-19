dc03a5a953af38856f2edd3e302439c9e12d5ab9
anl1313 <fxp671@alumni.ku.dk>
2024-06-19 09:03:55 +0200

    rettelser

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,6 +1,7 @@
 from types import SimpleNamespace
 import numpy as np
 from scipy import optimize
+import matplotlib.pyplot as plt
 
 class ExchangeEconomyClass:
 
@@ -146,6 +147,7 @@ class ExchangeEconomyClass:
         self.sol.x1 = result.x[0]
         self.sol.x2 = result.x[1]
         self.sol.u = self.utility_A(self.sol.x1, self.sol.x2)
+        return self.sol.x1, self.sol.x2
         
     def solve_socialplanner(self):
         """
@@ -209,16 +211,13 @@ class ExchangeEconomyClass:
     def equilibrium(self):
         """
         Args: p1, eps, maxiter
-        Returns: equilibrium price and allocation if in set C
+        Returns: equilibrium price and allocation for agent A
+
         """
         # a. use draw_pairs to get 50 pairs of endowments
         W_pairs = self.draw_pairs()
-        # b. initiate empty list for equilibrium prices
-        equilibrium_prices = []
         # c. initiate empty list for equilibrium allocations
         equilibrium_allocations = []
-        # d. initiate empty list for equilibrium utilities
-        equilibrium_utilities = []
         # e. loop through pairs of endowments
         for W in W_pairs:
             # i. set endowments
@@ -226,27 +225,353 @@ class ExchangeEconomyClass:
             self.par.w2A = W[1]
             self.par.w1B = 1 - W[0]
             self.par.w2B = 1 - W[1]
-            # ii. find equilibrium price using inital guess of 2.5
-            p1 = self.walras(2.5, eps=1e-8, maxiter=500)
-            # iii. find equilibrium allocation for A
-            self.solve()
-            # iv. append equilibrium price to equilibrium_prices
-            equilibrium_prices.append(p1)
-            # v. append equilibrium allocation to equilibrium_allocations
-            equilibrium_allocations.append((self.sol.x1, self.sol.x2))
-            # vi. append equilibrium utility to equilibrium_utilities
-            equilibrium_utilities.append(self.sol.u)
+            # ii. find equilibrium price using initial guess of 1
+            init_guess = 1
+            p1 = self.walras(init_guess, eps=1e-8, maxiter=500)
+            # iii. find demand for A
+            x1A, x2A = self.demand_A(p1)
+            # v. append equilibrium allocation for A to equilibrium_allocations
+            equilibrium_allocations.append((x1A, x2A))
+            #convert to x and y
+            x, y = zip(*equilibrium_allocations)
+            # vi. convert equilibrium_allocations to numpy array
+            x_list = np.array(x)
+            y_list = np.array(y)
+
+        return x_list, y_list
         
-    
+    def plot_walras_all(self):
+        """
+        Args:
+        Returns: plot of 50 pairs of endowments and equilibrium allocations for agent A
 
+        """
+        # a. use equilibrium to get equilibrium allocations
+        x_list, y_list = self.equilibrium()
+        # b. initiate figure
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
 
- 
-    
+        # b. figure set up
+        fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
+        ax_A = fig.add_subplot(1, 1, 1)
+
+        ax_A.set_xlabel("$x_1^A$",color="white")
+        ax_A.set_ylabel("$x_2^A$", color="white")
+
+        temp = ax_A.twinx()
+        temp.set_ylabel("$x_2^B$", color="white")
+        ax_B = temp.twiny()
+        ax_B.set_xlabel("$x_1^B$", color="white")
+
+        ax_A.tick_params(axis='x', colors='white')
+        ax_A.tick_params(axis='y', colors='white')
+        ax_B.tick_params(axis='x', colors='white')
+        temp.tick_params(axis='y', colors='white')
+
+        # limits
+        ax_B.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax_B.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax_B.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax_B.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax_A.set_xlim([-0.1, w1bar + 0.1])
+        ax_A.set_ylim([-0.1, w2bar + 0.1])    
+        ax_B.set_xlim([w1bar + 0.1, -0.1])
+        ax_B.set_ylim([w2bar + 0.1, -0.1])
         
+        # d. scatter plot of equilibrium allocations
+        ax_A.scatter(x_list, y_list, label='Equilibrium allocations')
+       
+        # f. show plot
+        plt.show()
 
+    def plot_walras(self):
+        # Make plot of convergence to equilibrium
+        # a. create empty list to store the errors
+        N = 75
+        excess_demands = []
 
+        # b. create a price range
+        price_range = np.linspace(0.1,2,N)
 
+        # c. loop over the price range
+        for p1 in price_range:
+            excess_demand = self.check_market_clearing(p1)
+            excess_demands.append(excess_demand)
+
+        # d. convert to numpy array
+        excess_demands = np.array(excess_demands)
+
+        # Plot the figure
+        plt.figure(figsize=(6, 6))
+
+        # Plot excess demand for good 1
+        plt.plot(np.linspace(0,2,N), excess_demands[:, 0], label='Excess demand for good 1')
+
+        # Plot excess demand for good 2
+        plt.plot(np.linspace(0,2,N), excess_demands[:, 1],label='Excess demand for good 2', linestyle='dashed')
+
+        # Add a horizontal line at zero to indicate market clearing
+        plt.axhline(0, color='black', linewidth=0.5)
+
+        plt.title('Walrasian Equilibrium')
+        plt.xlabel('p1')
+        plt.ylabel('Market error')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def plot_pareto(self):
+        utility_init_A = self.utility_A(self.par.w1A, self.par.w2A) #calling utility function in py-file
+        print(f"The utility of the bundle (x1A = {self.par.w1A}, x2A = {self.par.w2A}) is: {utility_init_A:.3f}")
+
+        # b. consumer B
+        utility_init_B = self.utility_B(self.par.w1B, self.par.w2B) #calling utility function in py-file
+        print(f"The utility of the bundle (x1B = {self.par.w1B:.1f}, x2B = {self.par.w2B}) is: {utility_init_B:.3f}")
+        #create grid for x1a and x2a
+        N = 75
+        x1A_val = np.linspace(0,1,N)
+        x2A_val = np.linspace(0,1,N)
+
+        #create a set C
+        C = []
+
+        #loop through grid and append pareto optimal values to C
+        for x1A in x1A_val:
+            for x2A in x2A_val:
+                # a. calculate the corresponding consume for B that follows from walras' law:
+                x1B = 1-x1A
+                x2B = 1-x2A
+                # b. calculate utility at every point
+                utility_A=self.utility_A(x1A,x2A)
+                utility_B=self.utility_B(x1B,x2B)
+                # c. check if value is pareto
+                if utility_A >= utility_init_A and utility_B >= utility_init_B:
+                    # d. append to C if condition is satisfied
+                    C.append((x1A,x2A))
+        # Plotting the Edgeworth box
+        C = np.array(C)
+
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
+
+        # b. figure set up
+        fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
+        ax1 = fig.add_subplot(1, 1, 1)
+
+        ax1.set_xlabel("$x_1^A$", color="white")
+        ax1.set_ylabel("$x_2^A$", color="white")
+
+        temp = ax1.twinx()
+        temp.set_ylabel("$x_2^B$", color="white")
+        ax2 = temp.twiny()
+        ax2.set_xlabel("$x_1^B$", color="white")
+
+        # a. plot the pareto set
+        ax1.scatter(C[:, 0], C[:, 1], s=10, alpha=0.2, color='blue') 
+
+        # Initial endowments
+        ax1.scatter(self.par.w1A, self.par.w2A, marker="s", color="black", label="Initial endowment")
+
+        # c. create title
+        plt.title('Pareto optimal points', color="white")
+
+        # d. enable grid for primary axes 
+        ax1.grid(True)
+        plt.tight_layout()
+
+        # e. limits
+        ax1.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax1.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax1.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax1.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax1.set_xlim([-0.1, w1bar + 0.1])
+        ax1.set_ylim([-0.1, w2bar + 0.1])    
+        ax2.set_xlim([w1bar + 0.1, -0.1])
+        ax2.set_ylim([w2bar + 0.1, -0.1])
+
+        # f. add legend
+        ax1.legend(frameon=True, loc='center left', bbox_to_anchor=(1.3, 0.5))
+
+        # g. set tick parameter
+        ax1.tick_params(axis='x', colors='white')
+        ax1.tick_params(axis='y', colors='white')
+        ax2.tick_params(axis='x', colors='white')
+        temp.tick_params(axis='y', colors='white')
+
+        plt.show()
+
+    def market_maker(self):
+        # a. consumer A
+        utility_init_A = self.utility_A(self.par.w1A, self.par.w2A) 
+
+        # b. consumer B
+        utility_init_B = self.utility_B(self.par.w1B, self.par.w2B) 
         
+        #create grid for x1a and x2a
+        N = 75
+        x1A_val = np.linspace(0,1,N)
+        x2A_val = np.linspace(0,1,N)
+
+        #create a set C
+        C = []
+
+        #loop through grid and append pareto optimal values to C
+        for x1A in x1A_val:
+            for x2A in x2A_val:
+                # a. calculate the corresponding consume for B that follows from walras' law:
+                x1B = 1-x1A
+                x2B = 1-x2A
+                # b. calculate utility at every point
+                utility_A=self.utility_A(x1A,x2A)
+                utility_B=self.utility_B(x1B,x2B)
+                # c. check if value is pareto
+                if utility_A >= utility_init_A and utility_B >= utility_init_B:
+                    # d. append to C if condition is satisfied
+                    C.append((x1A,x2A))
+        C = np.array(C)
+
+        # a. define initial utility and allocation for A
+        max_utility_A = -np.inf 
+        best_allocation_A = None
+
+        # Loop through optimal allocations for good 1 and good 2
+        for allocation in C:
+            # b. find the utility
+            utility_A = self.utility_A(allocation[0], allocation[1])
+            # c. check if the utility is higher than the current max utility
+            if utility_A > max_utility_A:
+                max_utility_A = utility_A
+                best_allocation_A = allocation
+        a1 = best_allocation_A[0]
+        a2 = best_allocation_A[1]
+        print(f'Looping through the set C, the utility is maximised for agent A when x1 = {a1:.3f} and x2 = {a2:.3f} with u = {self.utility_A(a1,a2):.3f}')
+        return a1, a2
+
+    def plot_all(self):
+        utility_init_A = self.utility_A(self.par.w1A, self.par.w2A) #calling utility function in py-file
+        #print(f"The utility of the bundle (x1A = {self.par.w1A}, x2A = {self.par.w2A}) is: {utility_init_A:.3f}")
+
+        # b. consumer B
+        utility_init_B = self.utility_B(self.par.w1B, self.par.w2B) #calling utility function in py-file
+        #print(f"The utility of the bundle (x1B = {self.par.w1B:.1f}, x2B = {self.par.w2B}) is: {utility_init_B:.3f}")
+        #create grid for x1a and x2a
+        N = 75
+        x1A_val = np.linspace(0,1,N)
+        x2A_val = np.linspace(0,1,N)
+
+        #create a set C
+        C = []
+
+        #loop through grid and append pareto optimal values to C
+        for x1A in x1A_val:
+            for x2A in x2A_val:
+                # a. calculate the corresponding consume for B that follows from walras' law:
+                x1B = 1-x1A
+                x2B = 1-x2A
+                # b. calculate utility at every point
+                utility_A=self.utility_A(x1A,x2A)
+                utility_B=self.utility_B(x1B,x2B)
+                # c. check if value is pareto
+                if utility_A >= utility_init_A and utility_B >= utility_init_B:
+                    # d. append to C if condition is satisfied
+                    C.append((x1A,x2A))
+        # Plotting the Edgeworth box
+        C = np.array(C)
+
+        # Show different allocations in the Edgeworth box
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
+
+        # b. figure set up
+        fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
+        ax_A = fig.add_subplot(1, 1, 1)
+
+        ax_A.set_xlabel("$x_1^A$",color="white")
+        ax_A.set_ylabel("$x_2^A$", color="white")
+
+        temp = ax_A.twinx()
+        temp.set_ylabel("$x_2^B$", color="white")
+        ax_B = temp.twiny()
+        ax_B.set_xlabel("$x_1^B$", color="white")
+
+        ax_A.tick_params(axis='x', colors='white')
+        ax_A.tick_params(axis='y', colors='white')
+        ax_B.tick_params(axis='x', colors='white')
+        temp.tick_params(axis='y', colors='white')
+
+        # A
+        # a. initial endowment
+        ax_A.scatter(self.par.w1A,self.par.w2A,marker='s',color='black',label='initial endowment')
+
+        # b. 3
+        ax_A.scatter(x1A,x2A,marker='s',color='orange',label='3 - Walrasian equilibrium')
+
+        # c. 4a
+        ax_A.scatter(optimal_allocation[0], optimal_allocation[1], marker='s',color='navy',label='4a - Any price in P1')
+
+        # d. 4b 
+        ax_A.scatter(opt_best_scipy[0], opt_best_scipy[1] ,marker='s',color='green',label='4b - Any positive price')
+
+        # e. 5a 
+        ax_A.scatter(self.a1, self.a2 ,marker='s',color='gray',label='5a - Choice set restricted to C')
+
+        # f. 5b
+        ax_A.scatter(self.solve.x1,self.solve.x2,marker='s',color='yellow',label='5b - Unrestricted choice set')
+
+        # g. 6a
+        ax_A.scatter(self.solve_socialplanner.x1,self.solve_socialplanner.x2 ,marker='s',color='red',label='6a - Social planner solution')
+
+        # Possible allocations
+        ax_A.scatter(C[:, 0], C[:, 1], s=15, color='blue', alpha=0.2, label='Pareto allocations')
+
+        # limits
+        ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax_A.set_xlim([-0.1, w1bar + 0.1])
+        ax_A.set_ylim([-0.1, w2bar + 0.1])    
+        ax_B.set_xlim([w1bar + 0.1, -0.1])
+        ax_B.set_ylim([w2bar + 0.1, -0.1])
+
+        ax_A.legend(frameon=True,loc='center right',bbox_to_anchor=(2.0,0.5));
+    def set_C(self):
+                # a. consumer A
+        utility_init_A = self.utility_A(self.par.w1A, self.par.w2A) 
+
+        # b. consumer B
+        utility_init_B = self.utility_B(self.par.w1B, self.par.w2B) 
+        
+        #create grid for x1a and x2a
+        N = 75
+        x1A_val = np.linspace(0,1,N)
+        x2A_val = np.linspace(0,1,N)
+
+        #create a set C
+        C = []
+
+        #loop through grid and append pareto optimal values to C
+        for x1A in x1A_val:
+            for x2A in x2A_val:
+                # a. calculate the corresponding consume for B that follows from walras' law:
+                x1B = 1-x1A
+                x2B = 1-x2A
+                # b. calculate utility at every point
+                utility_A= self.utility_A(x1A,x2A)
+                utility_B= self.utility_B(x1B,x2B)
+                # c. check if value is pareto
+                if utility_A >= utility_init_A and utility_B >= utility_init_B:
+                    # d. append to C if condition is satisfied
+                    C.append((x1A,x2A))
+        C = np.array(C)
+        return C
 
 
         
